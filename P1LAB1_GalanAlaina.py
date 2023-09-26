d19f1d9c997ac55a3f082cddd87d268cdcc556f6
AlainaGalan <146121160+AlainaGalan@users.noreply.github.com>
2023-09-26 13:23:51 -0400

    Add files via upload
    P1LAB1 Alaina Galan

---
+++ P1LAB1_GalanAlaina.py
@@ -0,0 +1,7 @@
+# Alaina
+# Sep 26, 2023
+# This code takes user input for your name and displays a sentence using the user input
+
+user_name = input("Enter your name: ")
+
+print("Hello", user_name, "and welcome to CS Online!")
