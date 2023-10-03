ff74340e21ada703b9862eb6f1e8c4849120dc21
AlainaGalan <146121160+AlainaGalan@users.noreply.github.com>
2023-10-03 13:18:40 -0400

    Add files via upload
    P2LAB2

---
+++ P2LAB2_GalanAlaina.py
@@ -0,0 +1,12 @@
+# Alaina Galan
+
+num1 = float(input("Enter number: "))
+num2 = float(input("Enter number: "))
+num3 = float(input("Enter number: "))
+num4 = float(input("Enter number: "))
+
+product= num1 * num2 * num3 * num4
+avg = (num1 +num2 + num3 + num4) /4
+
+print(f'{product:.0f} {avg:.0f}')
+print(f'{product:.3f} {avg:.3f}')
