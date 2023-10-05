3066b534f79b2f4cb2b8e50ffc81cefd9c4f9f6f
AlainaGalan <146121160+AlainaGalan@users.noreply.github.com>
2023-10-05 13:20:11 -0400

    P2LAB1_GalanAlaina.py

--- P2LAB1_GalanAlaina.py
+++ P2LAB1_GalanAlaina.py
@@ -1,11 +1,16 @@
 # Alaina Galan
 # Oct 2, 2023
+# Use floats and expressions to calcukate gas cost
 
+#create input variables
 milesPerGallon = float(input("Type miles per gallon: "))
 dollarsPerGallon = float(input("Type dollars per gallon: "))
 
-cost20 = 20 * dollarsPerGallon/milesPerGallon
-cost75 = 75 * dollarsPerGallon/milesPerGallon
-cost500 = 500 * dollarsPerGallon/milesPerGallon
+#Calculate gas cost based off gallons needed & price per gallon
+#Calculate for 20miles, 75miles, 500 miles
+cost20 = (20/milesPerGallon) * dollarsPerGallon
+cost75 = (75/milesPerGallon) * dollarsPerGallon
+cost500 = (500/milesPerGallon) * dollarsPerGallon
 
+#Ouput values using f'string & format floats
 print(f'{cost20:.2f} {cost75:.2f} {cost500:.2f}')
