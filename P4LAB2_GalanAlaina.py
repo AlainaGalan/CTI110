02f3c2c8c28b49c5c1583e731937b5a108bd3caa
AlainaGalan <146121160+AlainaGalan@users.noreply.github.com>
2023-10-31 14:08:24 -0400

    Add files via upload

---
+++ P4LAB2_GalanAlaina.py
@@ -0,0 +1,30 @@
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+
+if number1 <= number2:
+    while number1 <= number2:
+        print(number1, end=" ")
+        number1 += 5
+else:
+    print("Second integer can't be less than the first.")
+
+# For loop
+
+print("")
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+if number1 <= number2:
+    for item in range(number1, number2 + 1,5):
+        print(item, end =" ")
+            
+else:
+    while number1 > number2:
+       number2 = int(input("Enter another number: "))
+        for item in range(number1, number2 + 1,5):
+            print(item, end =" ")
+
+  
+
+        
+    
+    
